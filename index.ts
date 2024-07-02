03afd64f7c685635e555e4d61b02f555a8464ee5
yuniruyuni <yuniruyuni@gmail.com>
2024-07-02 17:15:26 +0900

    feat: remove v0, v1 and export as default

---
+++ index.ts
@@ -0,0 +1,5 @@
+export * from "./lib/event";
+export * from "./lib/environment";
+export * from "./lib/component";
+export * from "./lib/fetch";
+export * from "./lib/config";
--- v0/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./event";
-export * from "./environment";
-export * from "./component";
-export * from "./fetch";
-export * from "./config";
